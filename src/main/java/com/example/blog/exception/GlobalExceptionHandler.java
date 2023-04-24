bd8569fa13c5a76b11f50c1d303f665f2438afa5
shubham92000 <sbhsingh616@gmail.com>
2023-04-24 15:26:15 +0530

    in memory authentication

--- src/main/java/com/example/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/blog/exception/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 import java.util.Date;
 import java.util.HashMap;
@@ -54,6 +56,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                         webRequest.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(
+            AccessDeniedException exception,
+            WebRequest webRequest
+    ){
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), exception.getMessage(),
+                        webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
