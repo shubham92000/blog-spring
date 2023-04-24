0356b75a1e4f59889001b6e6e1f02721c349beba
shubham92000 <sbhsingh616@gmail.com>
2023-04-24 13:10:24 +0530

    added req validation for posts api

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.blog.payload.PostDto;
 import com.example.blog.payload.PostResponse;
 import com.example.blog.service.PostService;
 import com.example.blog.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
          return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -41,7 +42,7 @@ public class PostController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable("id") long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable("id") long id){
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
--- src/main/java/com/example/blog/payload/PostDto.java
+++ src/main/java/com/example/blog/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.example.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,8 +9,17 @@ import java.util.Set;
 @Data
 public class PostDto {
     private long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "post title should have at least 2 characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
+
     private Set<CommentDto> comments;
 }
